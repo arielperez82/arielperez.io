a85ac5af99e2af65a242f8c592365974775c82a8
Ariel Perez <ariel.x.perez@gmail.com>
2025-04-23 00:21:02 +0200

    Updated deprecated implementation for supabase middleware

--- webapp/src/middleware.ts
+++ webapp/src/middleware.ts
@@ -14,24 +14,16 @@ export async function middleware(request: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value
-        },
-        set(name: string, value: string, options: any) {
-          response.cookies.set({
-            name,
-            value,
-            ...options,
+        getAll: () => Array.from(request.cookies.getAll()).map(({ name, value }) => ({
+          name,
+          value
+        })),
+        setAll: (cookies) => {
+          cookies.forEach((cookie) => {
+            response.cookies.set(cookie)
           })
-        },
-        remove(name: string, options: any) {
-          response.cookies.set({
-            name,
-            value: '',
-            ...options,
-          })
-        },
-      },
+        }
+      }
     }
   )
 
