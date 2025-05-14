6dc62747800f41f160c8b238e2c3eee734387fd5
Ariel Perez <ariel.x.perez@gmail.com>
2025-05-14 22:09:08 +0200

    Fixed formatting and added line about direct hiring

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,17 @@
 module.exports = {
-  content: [
-    './src/**/*.{astro,js,jsx,ts,tsx,mdx}',
-    './public/**/*.html',
-  ],
+  content: ['./src/**/*.{astro,js,jsx,ts,tsx,mdx}', './public/**/*.html'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif']
       },
       colors: {
         sky: require('tailwindcss/colors').sky,
         gray: require('tailwindcss/colors').gray,
-        pink: require('tailwindcss/colors').pink,
-      },
-    },
+        pink: require('tailwindcss/colors').pink
+      }
+    }
   },
   plugins: [require('@tailwindcss/typography')],
-  darkMode: 'class',
-}; 
+  darkMode: 'class'
+}
