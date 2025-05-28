632a83685c4b6d56e2836ebfbfc5cc79dfed0709
Ariel Perez <ariel.x.perez@gmail.com>
2025-05-28 17:59:50 +0200

    Fixed style lint to not break the app

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ['stylelint-config-standard', 'stylelint-config-astro'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-astro',
+    'stylelint-config-tailwindcss'
+  ],
   rules: {
     'color-no-invalid-hex': true,
     'block-no-empty': true
